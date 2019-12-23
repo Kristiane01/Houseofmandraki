3b254d40755ce40489f2b3671cbf6ca14f1778fb
Kristiane <Kristiane@Jons-MacBook.local>
2019-12-23 15:09:27 -0600

    added memory

--- wp-config.php
+++ wp-config.php
@@ -94,6 +94,7 @@ $table_prefix = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', false );
+define( 'WP_MEMORY_LIMIT', '96M' );
 
 /* That's all, stop editing! Happy publishing. */
 
